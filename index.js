99409263817742782f2e79d57f3449e83961194c
mhmdiamd <darmawanilham34@gmail.com>
2023-04-03 06:07:43 +0700

    fix userChatRouter

--- index.js
+++ index.js
@@ -1,11 +1,10 @@
 import App from './app.js';
+import UserChatRouter from './src/UserChat/UserChat.routes.js';
 import GroupRouter from './src/group/group.routes.js';
 import MessageRouter from './src/message/message.routes.js';
 import TokenRouter from './src/token/token.routes.js';
 import AuthRouter from './src/user/auth/auth.routes.js';
 import UserRouter from './src/user/user.routes.js';
-import UserChatRouter from './src/UserChat/UserChat.routes.js';
-
 
 process.setMaxListeners(0);
 const app = new App([new AuthRouter(), new UserRouter(), new UserChatRouter(), new MessageRouter(),new TokenRouter(), new GroupRouter()], 3001);
