dc307c8833e0c2ad906487982bd7c11993bcdb36
mhmdiamd <darmawanilham34@gmail.com>
2023-04-07 06:27:35 +0700

    change routing

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 import App from './app.js';
-import UserChatRouter from './src/userChat/userChat.routes.js';
+// import UserChatRouter from './src/userChat/userChat.routes.js';
 import GroupRouter from './src/group/group.routes.js';
 import MessageRouter from './src/message/message.routes.js';
 import TokenRouter from './src/token/token.routes.js';
@@ -7,6 +7,6 @@ import AuthRouter from './src/user/auth/auth.routes.js';
 import UserRouter from './src/user/user.routes.js';
 
 process.setMaxListeners(0);
-const app = new App([new AuthRouter(), new UserRouter(),new UserChatRouter(), new MessageRouter(),new TokenRouter(), new GroupRouter()], 3001);
+const app = new App([new AuthRouter(), new UserRouter(), new MessageRouter(),new TokenRouter(), new GroupRouter()], 3001);
 
 app.listen();
